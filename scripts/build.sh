539be60ea3ce245d15692d3d753924153d0dd6a5
Daemon6109 <131625799+Daemon6109@users.noreply.github.com>
2025-03-20 21:22:55 -0500

    fixed scripts and restructured properly

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,9 @@ if [ ! -d "roblox_packages" ]; then
     bash scripts/install-packages.sh
 fi
 
+# Delete the dist folder
+rm -rf dist
+
 # Run rojo and darklua commands
 rojo sourcemap default.project.json -o sourcemap.json
 darklua process --config .darklua.json src/ dist/src
-rojo build build.project.json -o project.rbxm
